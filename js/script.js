90f2f14f5546c61a29e795e286aa49c2eb70730c
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-02-26 11:37:30 -0800

    updated fix for height of dropbar on mobile for projects

--- js/script.js
+++ js/script.js
@@ -6,11 +6,24 @@ function mediaQuery(query) {
 }
 const mobileSize = window.matchMedia("(max-width: 640px)")
 window.onload = function () {
+    let clicked
     mediaQuery(mobileSize)
+    document.getElementById('projects').addEventListener("click", function () {  
+        if (clicked == undefined) clicked = true
+        if (clicked) {
+            document.getElementById('dropbars').setAttribute('style', 'height: 317px')
+            clicked = false
+        }
+        else if (!clicked) {
+            document.getElementById('dropbars').setAttribute('style', 'height: 0px; transition-property: height; transition-duration: 200ms; transition-timing-function: linear;')
+            clicked = undefined
+        }
+    })
 }
 mobileSize.addEventListener("change", mediaQuery)
-$(document).ready(function() {
-    if(window.location.hash.length > 0) {
+$(document).ready(function () {
+    if (window.location.hash.length > 0) {
         window.scrollTo(0, $(window.location.hash).offset().top);
     }
 });
+
