7bcf9ec17736f8ef19bc49c07f823e9e71b4c752
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-02-26 17:20:46 -0800

    fix for about me section movement

--- js/script.js
+++ js/script.js
@@ -8,16 +8,40 @@ const mobileSize = window.matchMedia("(max-width: 640px)")
 window.onload = function () {
     let clicked
     mediaQuery(mobileSize)
-    document.getElementById('projects').addEventListener("click", function () {  
+
+    document.getElementById('infoSection').addEventListener("click", function () {
+        document.getElementById('aboutText').setAttribute('uk-scroll', 'offset: 300')
+        if(document.getElementById('dropbars').getAttributeNode('style').value == 'height: 0px; transition-property: height; transition-duration: 200ms; transition-timing-function: linear;' || document.getElementById('dropbars').getAttributeNode('style').value == 'height: 0px;') document.getElementById('aboutText').setAttribute('uk-scroll', 'offset: 0')
+    })
+
+    document.getElementById('projects').addEventListener("click", function () {
+        document.getElementById('aboutText').setAttribute('uk-scroll', 'offset: 300')
+        document.getElementById('dropbars').setAttribute('style', 'height: 317px; transition-property: height; transition-duration: 200ms; transition-timing-function: linear;')
+        //console.log(clicked)
+        document.getElementById('entireNav').addEventListener("click", function () {
+            clicked = undefined
+        })
+        document.getElementById('infoSection').addEventListener("click", function () {
+            document.getElementById('aboutText').setAttribute('uk-scroll', 'offset: 300')
+            clicked = undefined
+        })
+        document.getElementById('aboutText').addEventListener("click", function () {
+            console.log(document.getElementById('dropbars').getAttributeNode('style').value)
+            if(document.getElementById('dropbars').getAttributeNode('style').value == 'height: 0px; transition-property: height; transition-duration: 200ms; transition-timing-function: linear;' || document.getElementById('dropbars').getAttributeNode('style').value == 'height: 0px;') document.getElementById('aboutText').setAttribute('uk-scroll', 'offset: 0')
+            if (clicked) document.getElementById('aboutText').setAttribute('uk-scroll', 'offset: 300')
+            document.getElementById('dropbars').setAttribute('style', 'height: 0px; transition-property: height; transition-duration: 200ms; transition-timing-function: linear;')
+            clicked = undefined
+        })
         if (clicked == undefined) clicked = true
-        if (clicked) {
-            document.getElementById('dropbars').setAttribute('style', 'height: 317px')
+        else if (clicked) {
             clicked = false
         }
-        else if (!clicked) {
+        if (!clicked) {
+            document.getElementById('aboutText').setAttribute('uk-scroll', 'offset: 0')
             document.getElementById('dropbars').setAttribute('style', 'height: 0px; transition-property: height; transition-duration: 200ms; transition-timing-function: linear;')
             clicked = undefined
         }
+        console.log(clicked)
     })
 }
 mobileSize.addEventListener("change", mediaQuery)
